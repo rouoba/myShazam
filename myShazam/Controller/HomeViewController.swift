6d3e9eb892d4a6242d5fdc1809ebe6fce0f7dc53
rouoba <briceouoba@gmail.com>
2020-10-22 10:36:50 -0400

    Switch from  UIViewController to a UITableViewController to get custom cell working

--- myShazam/Controller/HomeViewController.swift
+++ myShazam/Controller/HomeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UITableViewController {
 
     let songList: [Song] = [
         Song(artistname: "Red Hot Chili Peppers", tracktitle: "Under The Bridge"),
@@ -15,32 +15,23 @@ class HomeViewController: UIViewController {
         Song(artistname: "Selena Gomez Feat. A$AP Rocky", tracktitle: "Good For You")
     ]
     
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet var myTableView: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        tableView.delegate = self
-//        tableView.dataSource = self
     }
 
-}
-
-extension HomeViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print("Hello")
-    }
-}
-
-extension HomeViewController: UITableViewDataSource {
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return songList.count
     }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> ViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ViewCell else {return ViewCell()}
-
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Row selected")
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> ViewCell {
+        guard let cell = myTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ViewCell else {return ViewCell()}
         cell.artist.text = songList[indexPath.row].artistname
         cell.songTitle.text = songList[indexPath.row].tracktitle
         return cell
